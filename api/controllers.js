2df8b36cf53c51852b06350e596d3c764010f12d
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2021-01-02 14:33:55 -0500

    removed sendMail ,Note:- was from forked code

--- api/controllers.js
+++ api/controllers.js
@@ -11,46 +11,6 @@ const controllers = {
         }
         res.status(200).json(aboutInfo)
     },
-    // function to send email form researchcollective.io's contact form to our inbox//
-    sendMail: async (req, res)  => {
-        const {fullName, email, message} = req.body
-        // email template for now - will be moving to 'templates' later
-        const emailContent = `
-          <p>${message}<p>
-          <p>
-            <p>From: <p>
-            <p>${fullName}</p>
-            <p>${email}</p>
-          </p>
-        `
-    
-        // Note to self:- Do not push code with SMTP config, put them in enviroment variable//
-        const transporter = await nodemailer.createTransport({
-            host: 'smtp-pulse.com',//transporter host name
-            port: 465,//transporter port
-            secure: true, //true for port 465, false for other ports
-            auth: {
-                user: process.env.SENDPULSE_ACCOUNT_EMAIL, // sendpulse account email
-                pass: process.env.SENDPULSE_ACCOUNT_PASSWORD, // sendpulse account password
-              },
-            //this is just for testing on localhost //
-              tls: {
-                rejectUnAuthorized: false
-              }
-        })
-        // send mail with transporter//
-        let mail = await transporter.sendMail({
-            from: `"Research Collective Contact" <${process.env.TRANSPORTER_EMAIL}>`,
-            to: process.env.TO_EMAIL, // can put multiple accounts//
-            subject: 'Research Colletive Contact Request',
-            // TODO: setting up some email template (maybe)
-            text: message, //plain text body//
-            html: emailContent//object from user's message
-        })
-        // Message sent//
-        console.log("Message sent: %s", mail.messageId, mail);
-        return res.status(200).json(`Email sent succesfully to ${process.env.TO_EMAIL}`)
-    },
     //function to add subscriber to mailchimp audience//
     subscribe: async (req, res) => {
       res.status(200).send('yay!!!')
